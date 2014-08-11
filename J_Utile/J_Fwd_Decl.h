22520da7308a3a8ab6d98c5507100f39ad9cd50a
JDRiley <jilesione@gmail.com>
2014-08-11 16:14:32 -0400

    Removed spurrious Windows.h include

--- J_Utile/J_Fwd_Decl.h
+++ J_Utile/J_Fwd_Decl.h
@@ -5,26 +5,10 @@
 
 #ifdef WIN32
 
-//#define _CRTDBG_MAP_ALLOC
-//#include <stdlib.h>
-//#include <crtdbg.h>
-//
-//#define CRT_MEMORY_CHECKING
 
 #define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
 
 #include <vld\vld.h> 
-
-
-//#ifdef _DEBUG   
-//#ifndef DBG_NEW      
-//#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )     
-//#define new DBG_NEW   
-//#endif
-//#endif  // _DEBUG
-
-
 
 
 #undef ERROR
